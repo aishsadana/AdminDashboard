51fb8a1d309563117ce4ca98aba7764eebcfd145
aishsadana <aishwaryasadana@gmail.com>
2019-04-27 16:45:46 +0530

    First commit

---
+++ home.php
@@ -0,0 +1,300 @@
+﻿<!DOCTYPE html>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "query";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+include('session.php');
+
+$usrname;
+$id;
+$sql="Select name,id from admin where username='".$login_session."'";
+			
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result)==1){
+	while($row=mysqli_fetch_assoc($result)){
+	$usrname=$row["name"];
+	$id=$row["id"];
+	}
+}
+
+
+session_start();
+?>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Admin Dashboard</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="css/font.css" rel="stylesheet" type="text/css">
+    <link href="css/font2.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+	<!-- JQuery DataTable Css -->
+    <link href="plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+    <!-- Morris Chart Css-->
+    <link href="plugins/morrisjs/morris.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="css/themes/all-themes.css" rel="stylesheet" />
+		 <style>
+	.button {
+	  padding: 5px 20px;
+	  font-size: 12px;
+	  text-align: center;
+	  cursor: pointer;
+	  outline: none;
+	  color: #fff;
+	  background-color:#4286f4;
+	  border: none;
+	  border-radius: 15px;
+	  box-shadow: 0 2px #0000cc;
+	}
+
+	.button:hover {background-color:#0000cc}
+
+	.button:active {
+	  background-color:#6666ff;
+	  box-shadow: 0 1px #666;
+	  transform: translateY(4px);
+	}
+	.select{
+		width:60%;
+		padding:2px 2px;
+		margin:0px;
+	}
+	</style>
+</head>
+
+<body class="theme-blue">
+
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Search Bar -->
+    <div class="search-bar">
+        <div class="search-icon">
+            <i class="material-icons">search</i>
+        </div>
+        <input type="text" placeholder="START TYPING...">
+        <div class="close-search">
+            <i class="material-icons">close</i>
+        </div>
+    </div>
+    <!-- #END# Search Bar -->
+    <!-- Top Bar -->
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+                <a href="javascript:void(0);" class="bars"></a>
+                <a class="navbar-brand" href="index.html"> ADMIN DASHBOARD </a>
+            </div>
+            
+        </div>
+    </nav>
+    <!-- #Top Bar -->
+    <section>
+        <!-- Left Sidebar -->
+        <aside id="leftsidebar" class="sidebar">
+            <!-- User Info -->
+            <div class="user-info">
+                <div class="image">
+                    <img src="images/college_logo.png" width="48" height="48" alt="Logo" />
+                </div>
+                <div class="info-container">
+                    <div class="name">
+					<?php 
+					
+					echo "Welcome ".$usrname;
+					?>
+					</div>
+					<div class="btn-group user-helper-dropdown">
+                        <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="logout.php"><i class="material-icons">input</i>Log Out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- #User Info -->
+            <!-- Menu -->
+            <div class="menu">
+                <ul class="list">
+                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="active">
+                        <a href="home.php">
+                            <i class="material-icons">layers</i>
+                            <span>Unanswered Queries</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="answered.php">
+                            <i class="material-icons">layers</i>
+                            <span>Answered Queries</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- #Menu -->
+            <!-- Footer -->
+            <div class="legal">
+                <div class="copyright">
+                    &copy; 2018 - 2019 <a href="javascript:void(0);"> MNNIT ALLAHABAD</a>.
+                </div>
+               
+            </div>
+            <!-- #Footer -->
+        </aside>
+        <!-- #END# Left Sidebar -->
+        <!-- Right Sidebar -->
+        <!-- #END# Right Sidebar -->
+    </section>
+
+	 <section class="content">
+        <div class="container-fluid">
+			 <!-- Exportable Table -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Unanswered Queries
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                                    <thead>
+                                        <tr>
+                                            <th>S.No.</th>
+                                            <th>Date</th>
+                                            <th>Query</th>
+                                            <th>User Email-Id</th>
+											<th>Answered</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>S.No.</th>
+                                            <th>Date</th>
+                                            <th>Query</th>
+                                            <th>User Email-Id</th>
+											<th>Answered</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php
+											$sql="Select date,query,email,queryId from queries where ans=0";
+											$result=mysqli_query($conn,$sql);
+											$no=1;
+											if(mysqli_num_rows($result)>0){
+												while($row=mysqli_fetch_assoc($result)){
+													echo "<tr>
+													<td>".$no."</td>
+													<td>".$row["date"]."</td>
+													<td>".$row["query"]."</td>
+													<td>".$row["email"]."</td>
+													<td>
+													<form action='' method='POST'>
+													<input type='hidden' name='queryId' value='".$row["queryId"]."'/>
+													<input type='checkbox' id='basic_checkbox_1' name='c1' value=1 />
+													<input type='submit' class='button' value='Save' name='submit'/>
+													</form>
+													</td>
+													</tr>";
+													$no++;
+												}
+											}
+								
+										?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Exportable Table -->
+        </div>
+    </section>
+
+					
+        <!-- Jquery Core Js -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="plugins/node-waves/waves.js"></script>
+
+        <!-- Jquery DataTable Plugin Js -->
+    <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+    <!-- Custom Js -->
+    <script src="js/admin.js"></script>
+    <script src="js/pages/tables/jquery-datatable.js"></script>
+
+    <!-- Demo Js -->
+    <script src="js/demo.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,137 @@
+﻿<!DOCTYPE html>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "query";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+// username and password sent from form 
+  
+  $username = mysqli_real_escape_string($conn,$_POST['username']);
+  $password = mysqli_real_escape_string($conn,$_POST['password']); 
+								  
+  $sql = "SELECT id FROM admin WHERE username = '$username' and password = '$password'";
+  $result = mysqli_query($conn,$sql);
+  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  $active = $row['active'];
+				  
+  $count = mysqli_num_rows($result);
+	//$count=1;							  
+  // If result matched $myusername and $mypassword, table row must be 1 row
+									
+  if($count == 1) {
+	 //session_register("username");
+	 $_SESSION['login_user'] = $username;
+									 
+     header("location: home.php");
+  }
+  else {
+	$_SESSION['error'] = "Your Username or Password is invalid ";
+  }
+}
+?>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Admin Dashboard</title>
+    <!-- Favicon-->
+    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="css/font.css" rel="stylesheet" type="text/css">
+    <link href="css/font2.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+            <a href="javascript:void(0);">Admin Login</a>
+        </div>
+        <div class="card">
+            <div class="body">
+                <form id="sign_in" method="POST" action="">
+                    <div class="msg">Sign in to start your session</div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">person</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">lock</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8 p-t-5">
+                            <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                            <label for="rememberme">Remember Me</label>
+                        </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+                        </div>
+                    </div>
+					</form>
+                    <div class="row">
+                        <div class="col-xs-12" style="color:red;">
+						<?php
+							if(isset($_SESSION['error'])){
+								echo $_SESSION['error'];
+								unset($_SESSION['error']);
+							}
+						?>                        
+						</div>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery Core Js -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../plugins/node-waves/waves.js"></script>
+
+    <!-- Validation Plugin Js -->
+    <script src="../../plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/examples/sign-in.js"></script>
+</body>
+
+</html>
